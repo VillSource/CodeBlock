b3e9e173e7ba74c820209654e7f55442006ff553
Villsource <anirut1800@gmail.com>
2023-02-06 19:33:53 +0700

    ci(editor): add monaco editor

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+import { BlockToolData } from '@editorjs/editorjs';
+
+/**
+ * tool Tool's input and output data format
+ */
+export interface toolData extends BlockToolData {}
+
+/**
+ * tool Tool's configuration object that passed through the initial Editor config
+ */
+export interface toolConfig {}
